d4840e96c7f09345225689fdfdce239e27a29cf7
Raghav Deshmukh <deshmukhraghav17@gmail.com>
2025-08-14 23:41:02 +0530

    url add

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 // settting up the file storage
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, "uploads");
+        cb(null, "/tmp");
     },
     filename: function(req, file, cb) {
         cb(null, file.originalname);
@@ -24,6 +24,11 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running on Render.");
+});
+
 app.post("/convertFile", upload.single("file"), (req, res, next) => {
     try {
         if (!req.file) {
@@ -31,22 +36,22 @@ app.post("/convertFile", upload.single("file"), (req, res, next) => {
                 message: "No file  uploaded",
             });
         }
+        // Input file path in /tmp
+        const inputPath = req.file.path;
         // Defining outout file path
-        let outoutPath = path.join(
-            __dirname,
-            "files",
-            `${req.file.originalname}.pdf`
-        );
-        docxToPDF(req.file.path, outoutPath, (err, result) => {
+        let outoutPath = path.join("/tmp", `${req.file.originalname}.pdf`);
+
+        docxToPDF(inputPath, outoutPath, (err, result) => {
             if (err) {
                 console.log(err);
                 return res.status(500).json({
                     message: "Error converting docx to pdf",
                 });
             }
-            res.download(outoutPath, () => {
-                console.log("file downloaded");
-            });
+            res.download(outoutPath, (err) => {
+        if (err) console.error("Error sending file:", err);
+        console.log("File converted and sent successfully");
+      });
         });
     } catch (error) {
         console.log(error);
